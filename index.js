276dd76f55935a646509130af55f47c9bfa2d45c
Hugo Zapata <>
2020-08-29 12:08:36 -0500

    Agregado getSvg static method

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 class DiagramEngine {
+    constructor() {}
 
-
-    constructor(){
-
-    }
-
-    static setEnginePath(path){
+    static setEnginePath(path) {
         DiagramEngine.enginePath = path;
     }
 
-    static setErrorHandler(cb){
+    static setErrorHandler(cb) {
         DiagramEngine.globalErrorHandler = cb;
     }
 
@@ -19,116 +15,157 @@ class DiagramEngine {
       @code: Diagram Code
       @theme: Diagram Theme
     */
-    static async renderDiagram({container,type,code,theme}){
-        if(!DiagramEngine.enginePath){
-            throw new Error("Diagram Engine path not set, use DiagramEngine.setEnginePath('path-to-engine')")
+    static async renderDiagram({ container, type, code, theme }) {
+        if (!DiagramEngine.enginePath) {
+            throw new Error(
+                "Diagram Engine path not set, use DiagramEngine.setEnginePath('path-to-engine')"
+            );
         }
-        let elem = typeof container === 'string' ? document.querySelector(container): container;
-        if(!elem){
-            throw new Error('Could not find container DOM element')
+        let elem =
+            typeof container === "string"
+                ? document.querySelector(container)
+                : container;
+        if (!elem) {
+            throw new Error("Could not find container DOM element");
         }
         //wait until we receive the 'ready'  message from the engine
         //only the 1st time
-        const iframe = container.querySelector('iframe[diagram-renderer]');
-        if(!iframe){
-            throw new Error('Please call DiagramEngine.init(container) to initialize the engine first')
+        const iframe = container.querySelector("iframe[diagram-renderer]");
+        if (!iframe) {
+            throw new Error(
+                "Please call DiagramEngine.init(container) to initialize the engine first"
+            );
         }
 
         //send the render message to the engine
-        iframe.contentWindow.postMessage({
-            type:'render-diagram',
-            data:{
-                type,
-                code,
-                theme
-            }
-        }, '*')
-
+        iframe.contentWindow.postMessage(
+            {
+                type: "render-diagram",
+                data: {
+                    type,
+                    code,
+                    theme,
+                },
+            },
+            "*"
+        );
     }
 
     /* Creates an iframe inside the container
        The iframe will be 100% width and height of the container */
-    static async init(elem){
-        return new Promise((resolve)=>{
+    static async init(elem) {
+        return new Promise((resolve) => {
             //Is already initialized? skip
-            let iframe = elem.querySelector('iframe[diagram-renderer]')
-            if(iframe){
-                 iframe.remove();
+            let iframe = elem.querySelector("iframe[diagram-renderer]");
+            if (iframe) {
+                iframe.remove();
             }
 
-
             //clear container
-            elem.innerHTML = '';
+            elem.innerHTML = "";
 
-            iframe = document.createElement('iframe');
-            iframe.style.width = "100%"
-            iframe.style.height = "100%"
+            iframe = document.createElement("iframe");
+            iframe.style.width = "100%";
+            iframe.style.height = "100%";
             iframe.style.border = "none";
             iframe.style.minWidth = "400px";
             iframe.style.minHeight = "400px";
-            iframe.setAttribute('diagram-renderer',"")
-            iframe.setAttribute('ready', "false")
-            iframe.src = DiagramEngine.enginePath
+            iframe.setAttribute("diagram-renderer", "");
+            iframe.setAttribute("ready", "false");
+            iframe.src = DiagramEngine.enginePath;
 
             /* Creamos handler para esperar mensaje de ready del engine
                cuando llegue resolvemos la promesa.
                Sólo cuando recibimos el evento ready se cumple
                la promesa
             */
-            const handler = (ev)=>{
-
-                if(ev.data && ev.data.type === 'diagram-render-engine-ready'){
-                    iframe.setAttribute('ready', "true")
+            const handler = (ev) => {
+                if (ev.data && ev.data.type === "diagram-render-engine-ready") {
+                    iframe.setAttribute("ready", "true");
                     resolve(iframe);
                 }
-            }
+            };
 
-            DiagramEngine.addMessageHandler('diagram-render-engine-ready', iframe, handler, true)
-            elem.appendChild(iframe)
-
-        })
+            DiagramEngine.addMessageHandler(
+                "diagram-render-engine-ready",
+                iframe,
+                handler,
+                true
+            );
+            elem.appendChild(iframe);
+        });
     }
 
+    static getSvg({ container }) {
+        return new Promise((resolve) => {
+            const iframe = container.querySelector("iframe[diagram-renderer]");
+            if (!iframe) {
+                throw new Error(
+                    "Please call DiagramEngine.init(container) to initialize the engine first"
+                );
+            }
+
+            const handler = (ev) => {
+                console.log("getSvg handler ev:", ev);
+                resolve(ev.data);
+            };
+            //Register the handler for the response (with once=true)
+            DiagramEngine.addMessageHandler(
+                "diagram-render-engine-ready",
+                iframe,
+                handler,
+                true
+            );
+
+            //send request to return the svg
+            iframe.contentWindow.postMessage(
+                {
+                    type: "request-rendered-svg",
+                },
+                "*"
+            );
+        });
+    }
 
     /*  El padre puede recibir mensajes de diferentes iframes
         usamos esta lista de handlers para que podamos ejecutar 
         codigo sólo para mensajes de un iframe especifico */
 
-    static addMessageHandler(eventName, iframe, cb, once=false){
-        DiagramEngine.handlers.push({eventName, iframe, cb})
+    static addMessageHandler(eventName, iframe, cb, once = false) {
+        DiagramEngine.handlers.push({ eventName, iframe, cb });
     }
 }
 
 /* Llamamos el handler del iframe apropiado */
-window.addEventListener('message', (ev)=>{
-    if(ev.data && ev.data.type) {
-        let handler = DiagramEngine.handlers.find((h)=>{
+window.addEventListener("message", (ev) => {
+    if (ev.data && ev.data.type) {
+        let handler = DiagramEngine.handlers.find((h) => {
             //Verificar si el ifram del handler corresponde
             //con la fuente de este evento
-            return h.eventName === ev.data.type &&
-                   h.iframe.contentWindow === ev.source
-        })
+            return (
+                h.eventName === ev.data.type &&
+                h.iframe.contentWindow === ev.source
+            );
+        });
 
         //Si hay un handler registrado para el iframe y el evento lo llamamos
         //Si es un handler "once" lo quitamos de la lista
-        if(handler){
-            handler.cb(ev)
-            if(handler.once){
-                const ix = DiagramEngine.handlers.indexOf(handler)
-                DiagramEngine.handlers.splice(ix, 1)
+        if (handler) {
+            handler.cb(ev);
+            if (handler.once) {
+                const ix = DiagramEngine.handlers.indexOf(handler);
+                DiagramEngine.handlers.splice(ix, 1);
             }
         }
-   
 
         //Ver si es un error para ejecutar el handler de error global
-        if(ev.data && ev.data.type === 'diagram-render-error'){
-            if(DiagramEngine.globalErrorHandler) {
+        if (ev.data && ev.data.type === "diagram-render-error") {
+            if (DiagramEngine.globalErrorHandler) {
                 DiagramEngine.globalErrorHandler(ev.data);
             }
         }
-
     }
-})
+});
 
 //Global error handler
 // window.addEventListener('message', (ev)=>{
@@ -140,7 +177,6 @@ window.addEventListener('message', (ev)=>{
 //     }
 // })
 
-
 DiagramEngine.handlers = [];
 
-export default DiagramEngine
+export default DiagramEngine;
